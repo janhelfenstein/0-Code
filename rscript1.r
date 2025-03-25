cedfb5bd0f9ad3439c62f581bedefa781e6caebf
Jan Helfenstein <jan_marvin@hotmail.com>
2025-03-25 22:11:21 +0100

    moved gapminder code to new quarto file: quarto1. updated plots in questionnaire-summary: pie chart and barplot. added factor variables to fix bar order in the barplot

--- rscript1.r
+++ rscript1.r
@@ -6,3 +6,20 @@ library(ggplot2)
 library(dplyr)
 
 data_2023 = read_csv("data/raw/SM10K-2023-raw.csv")
+
+
+# test basic course knowledge
+library(gapminder)
+
+gapminder_2007 <- gapminder |>
+    filter(year == 2007)
+
+ggplot(data = gapminder_2007,
+      mapping = aes(x = continent,
+                   y = lifeExp,
+                   fill = continent
+                   )
+      ) +
+    geom_boxplot()
+
+# somehow the plot doesn't appear
